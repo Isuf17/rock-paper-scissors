ffc3f870a84559758732b822fe4a852b866a9da4
Isuf17 <isuf17@gmail.com>
2022-06-16 13:52:14 +0100

    Finish simple game

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ function computerPlay() {
     return (options[Math.floor(Math.random() * 3)]);
 }
 
-function round(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+    let computerSelection = computerPlay();
     playerSelection = playerSelection.toLowerCase();
 
     if (computerSelection == 'rock') {
@@ -33,30 +34,76 @@ function round(playerSelection, computerSelection) {
     }
 }
 
-function game() {
-    let playerScore = 0
-    let computerScore = 0
+// function game() {
+//     let playerScore = 0
+//     let computerScore = 0
 
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt('Rock, paper, scissors?');
-        const computerSelection = computerPlay();
-        let result = round(playerSelection, computerSelection);
+//     for (let i = 0; i < 5; i++) {
+//         let playerSelection = prompt('Rock, paper, scissors?');
+//         const computerSelection = computerPlay();
+//         let result = round(playerSelection, computerSelection);
 
-        console.log(result)
+//         console.log(result)
 
-        if (result == 'Draw!') {
-            continue
-        } else if (result.slice(0,6) == 'You win') {
-            playerScore++
-        } else {
-            computerScore++
-        }
-    }
-    if (playerScore > computerScore) {
-        return "You win the game!"
-    } else if (computerScore < playerScore) {
-        return "You lose the game!"
+//         if (result == 'Draw!') {
+//             continue
+//         } else if (result.slice(0,6) == 'You win') {
+//             playerScore++
+//         } else {
+//             computerScore++
+//         }
+//     }
+//     if (playerScore > computerScore) {
+//         return "You win the game!"
+//     } else if (computerScore < playerScore) {
+//         return "You lose the game!"
+//     } else {
+//         return "Draw!"
+//     }
+// }
+
+
+let playerScore = 0;
+let computerScore = 0;
+const body = document.querySelector('body');
+
+const round = document.createElement('div');
+round.classList.add('round');
+
+const finish = document.createElement('div');
+finish.classList.add('end-game');
+
+const buttons = document.querySelectorAll('button');
+
+
+window.addEventListener('click', function (e) {
+    const playerSelection = e.target.className;
+    let round_result = playRound(playerSelection);
+
+    if (round_result == 'Draw!') {
+        round.textContent = round_result + " Player score =  " + playerScore + ", computer score = " + computerScore + ".";
+        body.appendChild(round);
+        return
+    } else if (round_result.slice(0,7) == 'You win') {
+        playerScore++
     } else {
-        return "Draw!"
+        computerScore++
     }
-}
+
+    round.textContent = round_result + " Player score =  " + playerScore + ", computer score = " + computerScore + ".";
+    body.appendChild(round);
+
+    if (playerScore == 5) {
+        finish.textContent = 'You Won!';
+        body.appendChild(finish);
+        buttons.forEach(button => {
+            button.disabled = true;
+        })
+    } else if (computerScore == 5) {
+        finish.textContent = 'You Lose!';
+        body.appendChild(finish);
+        buttons.forEach(button => {
+            button.disabled = true;
+        })
+    }
+})
